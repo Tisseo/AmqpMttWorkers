e928041f0c6a87d211e284f33e49fc08f1f52177
Vincent Degroote <vincent.degroote@canaltp.fr>
2014-06-26 06:22:20 +0200

    METH-254 added number of tries for a message. republish until nb tries is reached instead of nack.

--- pdfGenerationWorker.php
+++ pdfGenerationWorker.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is under development :p
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -21,7 +22,6 @@ use CanalTP\AMQPMttWorkers\TimetableMediaBuilder;
 $curlProxy = new CurlProxy();
 $pdfHashingLib = new PdfHashingLib($curlProxy);
 
-// $connection = new AMQPConnection(HOST, PORT, USER, PASS, VHOST);
 $channelLib = new Channel(HOST, USER, PASS, PORT, VHOST);
 $channel = $channelLib->getChannel();
 
@@ -29,64 +29,105 @@ list($queue_name, ,) = $channel->queue_declare($channelLib->getPdfGenQueueName()
 $channel->queue_bind($queue_name, $channelLib->getExchangeName(), "*.pdf_gen");
 
 $ttMediaBuilder = new TimetableMediaBuilder();
+// how many tries attempted for each message
+$nbTries = isset($argv[1]) && is_int($argv[1]) ?  $argv[1] : 3;
 
-$process_message = function($msg) use ($curlProxy, $pdfHashingLib, $ttMediaBuilder, $channelLib)
+$ackMessage = function ($msgToAck, $ackPayload) use ($channelLib){
+    $ackMsg = new AMQPMessage(
+        json_encode($ackPayload),
+        array(
+            'delivery_mode' => 2,
+            'content_type'  => 'application/json'
+        )
+    );
+    // publish to ack queue
+    $msgToAck->delivery_info['channel']->basic_publish(
+        $ackMsg, 
+        $channelLib->getExchangeName(), 
+        $msgToAck->get('reply_to'), 
+        true
+    );
+    echo " [x] Sent ",$msgToAck->get('reply_to'),':',print_r($ackPayload, true)," \n";
+    echo "\n--------\n";
+    // acknowledge broker
+    $msgToAck->delivery_info['channel']->basic_ack($msgToAck->delivery_info['delivery_tag']);
+};
+
+$process_message = function($msg) use ($curlProxy, $pdfHashingLib, $ttMediaBuilder, $channelLib, $ackMessage, $nbTries)
 {
-    $payload = json_decode($msg->body);
-    echo "\n--------\n";
-    print_r($payload);
-    echo "\n--------\n";
-    $html = $curlProxy->get($payload->url);
-    if (empty($html)) {
-        echo "Got empty response $html from server, url: " . ($payload->url);
+    try{
+        $payload = json_decode($msg->body);
         echo "\n--------\n";
-        $msg->delivery_info['channel']->basic_nack($msg->delivery_info['delivery_tag'], false, true);
-    } else {
-        $hash = $pdfHashingLib->getPdfHash($html, $payload->cssVersion);
-        echo "pdf hash: " . ($hash);
+        print_r($payload);
         echo "\n--------\n";
-        
-        if ($hash != $payload->pdfHash) {
-            $pdfGenerator = new PdfGenerator($curlProxy, $payload->pdfGeneratorUrl, false);
-            $pdfPath = $pdfGenerator->getPdf($payload->url, $payload->layoutParams->orientation);
-            echo "pdfPath: " . $pdfPath;
-            $filepath = $ttMediaBuilder->saveFile(
-                $pdfPath,
-                $payload->timetableParams->externalNetworkId,
-                $payload->timetableParams->externalRouteId,
-                $payload->timetableParams->externalStopPointId,
-                $payload->timetableParams->seasonId
-            );
-            echo "Generation result :" . $filepath ? $filepath : 'NOK';
-            echo "\n--------\n";
-        }
-        
-        // acknowledgement part
-        // push ack data into expected queue
-        if (isset($filepath)) {
-            $payload->generated = true;
-            $payload->generationResult = new \stdClass;
-            $payload->generationResult->filepath = $filepath;
-            $payload->generationResult->pdfHash = $hash;
-            $payload->generationResult->created = time();
+        $html = $curlProxy->get($payload->url);
+        if (empty($html)) {
+            throw new \Exception("Got empty response $html from server, url: " . ($payload->url));
         } else {
-            $payload->generated = false;
+            $hash = $pdfHashingLib->getPdfHash($html, $payload->cssVersion);
+            echo "pdf hash: " . ($hash);
+            echo "\n--------\n";
+            
+            if ($hash != $payload->pdfHash) {
+                $pdfGenerator = new PdfGenerator($curlProxy, $payload->pdfGeneratorUrl, false);
+                $pdfPath = $pdfGenerator->getPdf($payload->url, $payload->layoutParams->orientation);
+                echo "pdfPath: " . $pdfPath;
+                $filepath = $ttMediaBuilder->saveFile(
+                    $pdfPath,
+                    $payload->timetableParams->externalNetworkId,
+                    $payload->timetableParams->externalRouteId,
+                    $payload->timetableParams->externalStopPointId,
+                    $payload->timetableParams->seasonId
+                );
+                echo "Generation result :" . $filepath ? $filepath : 'NOK';
+                echo "\n--------\n";
+            }
+            
+            // acknowledgement part
+            // push ack data into expected queue
+            if (isset($filepath)) {
+                $payload->generated = true;
+                $payload->generationResult = new \stdClass;
+                $payload->generationResult->filepath = $filepath;
+                $payload->generationResult->pdfHash = $hash;
+                $payload->generationResult->created = time();
+            } else {
+                $payload->generated = false;
+            }
+            $ackMessage($msg, $payload);
+            
         }
-        $ackMsg = new AMQPMessage(
-            json_encode($payload),
-            array(
-                'delivery_mode' => 2,
-                'content_type'  => 'application/json'
-            )
-        );
-        // publish to ack queue
-        $msg->delivery_info['channel']->basic_publish($ackMsg, $channelLib->getExchangeName(), $msg->get('reply_to'), true);
-        echo " [x] Sent ",$msg->get('reply_to'),':',print_r($payload, true)," \n";
+    } catch(\Exception $e){
+        echo "\n----  Error catched  ----\n";
+        echo $e->getMessage() . "\n";
         echo "\n--------\n";
-        // acknowledge broker
-        $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+        $payload = json_decode($msg->body);
+        // is it the first try? if so initialize nbTries variable
+        if (!isset($payload->nbTries)) {
+            $payload->nbTries = 0;
+        }
+        $payload->nbTries++;
+        echo "\n----  Nb tries: ", $payload->nbTries ,"  ----\n";
+        if ($payload->nbTries == $nbTries) {
+            echo "\n----  Message Redelivered in error  ----\n";
+            $payload->generated = false;
+            $payload->error = $e->getMessage();
+            $ackMessage($msg, $payload);
+        } else {
+            $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+            // republish it with new payload
+            echo "\n----  Republish to Retry  ----\n";
+            $newMsg = new AMQPMessage(
+                json_encode($payload),
+                array(
+                    'delivery_mode' => 2,
+                    'content_type'  => 'application/json',
+                    'reply_to'      => $msg->get('reply_to')
+                )
+            );
+            $channelLib->getChannel()->basic_publish($newMsg, $channelLib->getExchangeName(), $msg->delivery_info['routing_key'], true);
+        }
     }
-    // sleep(10);
 };
 $channel->basic_qos(null, 1, null);
 $channel->basic_consume($queue_name, 'pdfWorker', false, false, false, false, $process_message);
